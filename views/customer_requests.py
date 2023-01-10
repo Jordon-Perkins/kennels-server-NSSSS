8e04216c1f3d8d89ef51c78b7588973650aa3164
Jordon Perkins <jordon.rubel@gmail.com>
2023-01-10 11:32:55 -0600

    finished kennel server main portion

--- views/customer_requests.py
+++ views/customer_requests.py
@@ -38,3 +38,28 @@ def create_customer(customer):
 
     # Return the dictionary with `id` property added
     return customer
+
+def delete_customer(id):
+    # Initial -1 value for animal index, in case one isn't found
+    customer_index = -1
+
+    # Iterate the ANIMALS list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, customer in enumerate(CUSTOMERS):
+        if customer["id"] == id:
+            # Found the animal. Store the current index.
+            customer_index = index
+
+    # If the animal was found, use pop(int) to remove it from list
+    if customer_index >= 0:
+        CUSTOMERS.pop(customer_index)
+
+
+def update_customer(id, new_customer):
+    # Iterate the ANIMALS list, but use enumerate() so that
+    # you can access the index value of each item.
+    for index, customer in enumerate(CUSTOMERS):
+        if customer["id"] == id:
+            # Found the animal. Update the value.
+            CUSTOMERS[index] = new_customer
+            break
