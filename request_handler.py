1293e1e904c971f27bdc2a825474f4c6f072cc75
Jordon Perkins <jordon.rubel@gmail.com>
2023-01-19 14:22:31 -0600

    add get with other keys functions to requests

--- request_handler.py
+++ request_handler.py
@@ -1,10 +1,12 @@
 import json
+from urllib.parse import urlparse, parse_qs
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from views import (get_all_animals, get_single_animal, get_all_customers, 
 get_single_customer, get_all_employees, get_all_locations, get_single_employee, 
 get_single_location, create_animal, create_customer, create_employee, create_location, 
 delete_animal, delete_customer, delete_employee, delete_location, update_animal, 
-update_location, update_customer, update_employee)
+update_location, update_customer, update_employee, get_customers_by_email, get_animals_by_location_id,
+get_employees_by_location_id, get_animals_by_status)
 
 
 
@@ -20,68 +22,106 @@ class HandleRequests(BaseHTTPRequestHandler):
     """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
     """
 
+# replace the parse_url function in the class
     def parse_url(self, path):
-        # Just like splitting a string in JavaScript. If the
-        # path is "/animals/1", the resulting list will
-        # have "" at index 0, "animals" at index 1, and "1"
-        # at index 2.
-        path_params = path.split("/")
+        """Parse the url into the resource and id"""
+        parsed_url = urlparse(path)
+        path_params = parsed_url.path.split('/')  # ['', 'animals', 1]
         resource = path_params[1]
-        id = None
 
-        # Try to get the item at index 2
+        if parsed_url.query:
+            query = parse_qs(parsed_url.query)
+            return (resource, query)
+
+        pk = None
         try:
-            # Convert the string "1" to the integer 1
-            # This is the new parseInt()
-            id = int(path_params[2])
-        except IndexError:
-            pass  # No route parameter exists: /animals
-        except ValueError:
-            pass  # Request had trailing slash: /animals/
-
-        return (resource, id)  # This is a tuple
+            pk = int(path_params[2])
+        except (IndexError, ValueError):
+            pass
+        return (resource, pk)
 
     def do_GET(self):
-        """Handles GET requests to the server"""
-        
-        status_code = 200
-        response = {}  # Default response
+        self._set_headers(200)
 
-        # Parse the URL and capture the tuple that is returned
-        (resource, id) = self.parse_url(self.path)
+        response = {}
 
-        if resource == "animals":
-            if id is not None:
-                response = get_single_animal(id)
-                if response is None:
-                    status_code = 404
-                    response = { "message": f"Animal {id} has already gone home" }
-            else:
-                response = get_all_animals()
-            
-        elif resource == "customers":
-            if id is not None:
-                response = get_single_customer(id)
+        # Parse URL and store entire tuple in a variable
+        parsed = self.parse_url(self.path)
 
-            else:
-                response = get_all_customers()
+        # If the path does not include a query parameter, continue with the original if block
+        if '?' not in self.path:
+            ( resource, id ) = parsed
 
-        elif resource == "employees":
-            if id is not None:
-                response = get_single_employee(id)
+            if resource == "animals":
+                if id is not None:
+                    response = get_single_animal(id)
+                else:
+                    response = get_all_animals()
+            elif resource == "customers":
+                if id is not None:
+                    response = get_single_customer(id)
+                else:
+                    response = get_all_customers()
 
-            else:
-                response = get_all_employees()
+        else: # There is a ? in the path, run the query param functions
+            (resource, query) = parsed
 
-        elif resource == "locations":
-            if id is not None:
-                response = get_single_location(id)
+            # see if the query dictionary has an email key
+            if query.get('email') and resource == 'customers':
+                response = get_customers_by_email(query['email'][0])
+            elif query.get('location_id') and resource == 'animals':
+                response = get_animals_by_location_id(query['location_id'][0])
+            elif query.get('location_id') and resource == 'employees':
+                response = get_employees_by_location_id(query['location_id'][0])
+            elif query.get('status') and resource == 'animals':
+                response = get_animals_by_status(query['status'][0])
 
-            else:
-                response = get_all_locations()
 
-        self._set_headers(status_code)
         self.wfile.write(json.dumps(response).encode())
+
+
+
+    # def do_GET(self):
+    #     """Handles GET requests to the server"""
+        
+    #     status_code = 200
+    #     response = {}  # Default response
+
+    #     # Parse the URL and capture the tuple that is returned
+    #     (resource, id) = self.parse_url(self.path)
+
+    #     if resource == "animals":
+    #         if id is not None:
+    #             response = get_single_animal(id)
+    #             if response is None:
+    #                 status_code = 404
+    #                 response = { "message": f"Animal {id} has already gone home" }
+    #         else:
+    #             response = get_all_animals()
+            
+    #     elif resource == "customers":
+    #         if id is not None:
+    #             response = get_single_customer(id)
+
+    #         else:
+    #             response = get_all_customers()
+
+    #     elif resource == "employees":
+    #         if id is not None:
+    #             response = get_single_employee(id)
+
+    #         else:
+    #             response = get_all_employees()
+
+    #     elif resource == "locations":
+    #         if id is not None:
+    #             response = get_single_location(id)
+
+    #         else:
+    #             response = get_all_locations()
+
+    #     self._set_headers(status_code)
+    #     self.wfile.write(json.dumps(response).encode())
 
     def do_PUT(self):
         self._set_headers(204)
--- views/customer_requests.py
+++ views/customer_requests.py
@@ -58,13 +58,13 @@ def get_single_customer(id):
         # into the SQL statement.
         db_cursor.execute("""
         SELECT
-            a.id,
-            a.name,
-            a.address,
-            a.email,
-            a.password
-        FROM customer a
-        WHERE a.id = ?
+            c.id,
+            c.name,
+            c.address,
+            c.email,
+            c.password
+        FROM customer c
+        WHERE c.id = ?
         """, ( id, ))
 
         # Load the single result into memory
@@ -75,7 +75,32 @@ def get_single_customer(id):
 
         return customer.__dict__
 
+def get_customers_by_email(email):
 
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        select
+            c.id,
+            c.name,
+            c.address,
+            c.email,
+            c.password
+        from Customer c
+        WHERE c.email = ?
+        """, ( email, ))
+
+        customers = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            customer = Customer(row['id'], row['name'], row['address'], row['email'] , row['password'])
+            customers.append(customer.__dict__)
+
+    return customers
 
 
 # def get_single_customer(id):
